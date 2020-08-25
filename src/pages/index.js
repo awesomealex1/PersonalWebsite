1459c7b1abd877862b4e16ca03044ecbf78c3da2
Alex Murphy <lordpizza11@gmail.com>
2020-08-25 12:53:20 +0200

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<div>
+			<h1>Hello!</h1>
+			<FontAwesomeIcon icon={faGithub} size="5x"/>
+		</div>
+	);
 }
