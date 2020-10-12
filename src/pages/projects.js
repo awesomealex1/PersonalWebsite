fb66952ac8d6ef31b521d2445c0597d90593197f
Alex Murphy <lordpizza11@gmail.com>
2020-10-12 13:38:08 +0100

    changed links

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,13 +13,13 @@ export default function Projects() {
             <Link to="/"> (or go back)</Link>
             </h1>
             
-            <Project name="Search Algorithm Visualiser" link="https://alexmurphy.me/" image={Search} description="
+            <Project name="Search Algorithm Visualiser" link="https://alexmurphy.me/SearchAlgorithm/" image={Search} description="
             A website that visualises search algorithms (Depth First Search, Breadth First Search, A* Search). Features include a resizable grid, possibility to add walls, move start and endpoints and change speed of algorithm. Built with React. " 
             github="https://github.com/awesomealex1/Search"></Project>
 
             <Project name="This Website" link="https://alexmurphy.me/" image={PersonalWebsite} description="
             The website you are on right now! Built with gatsby. " 
-            github="https://github.com/awesomealex1/Search"></Project>
+            github="https://github.com/awesomealex1/PersonalWebsite"></Project>
 
             <Project name="Mandelbrot Set Image Generator" link="https://github.com/awesomealex1/Mandelbrot-Set-Image-Generator" image={Mandelbrot} description="
             A small Python App to generate images of the Mandelbrot Set. You can configure the colors of the images, the resolution and the accuracy of the image generated. " 
